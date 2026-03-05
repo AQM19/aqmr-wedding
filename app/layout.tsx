43820474e3d9af1e6e7b49566c6e9f5fbdcc5d39
Aaron <aquintanalm.dev@gmail.com>
2026-03-05 14:51:20 +0100

    Generación de metadatos

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,73 @@ import { playfair } from "@/lib";
 import { montserrat } from "@/lib/fonts/montserrat";
 import type { Metadata } from "next";
 import { Suspense } from "react";
+import { host } from "./config";
 
 export const metadata: Metadata = {
   title: "Aarón & Merce - Nuestra boda",
   description: `¡Nos casamos! Celebra con nosotros nuestro gran día`,
   authors: [
     {
-      name: "Aarón Quintanal Martín",
-      url: "https://p-qfoil.vercel.app/es"
+      name: 'Aarón Quintanal Martín',
+      url: host
     }
-  ]
+  ],
+  keywords: ["Aarón Quintanal Martín", "Mercedes Ríos Gutiérrez", "boda", "celebración", "amor", "compromiso", "familia", "amigos", "evento", "invitación", "ceremonia", "fiesta", "recuerdos", "momentos especiales"],
+  openGraph: {
+    title: "Aarón & Merce - Nuestra boda",
+    description: `¡Nos casamos! Celebra con nosotros nuestro gran día`,
+    url: host,
+    type: "website",
+    images: [
+      {
+        url: `${host}/imgs/romantic-wedding-venue-sunset-elegant.jpg`,
+        width: 800,
+        height: 600,
+        alt: "Imagen de la boda de Aarón y Merce",
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Aarón & Merce - Nuestra boda",
+    description: `¡Nos casamos! Celebra con nosotros nuestro gran día`,
+    site: '@AQuintanalMDev',
+    creator: '@AQuintanalMDev',
+    images: [
+      `${host}/imgs/romantic-wedding-venue-sunset-elegant.jpg`
+    ]
+  },
+  alternates: {
+    canonical: host,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  appleWebApp: {
+    title: 'Aarón & Merce - Nuestra boda',
+    statusBarStyle: 'black-translucent',
+    startupImage: [
+      '/imgs/romantic-wedding-venue-sunset-elegant.jpg',
+      {
+        url: '/assets/imgs/romantic-wedding-venue-sunset-elegant.jpg',
+        media: '(device-width: 768px) and (device-height: 1024px)',
+      },
+    ],
+  },
+  assets: [
+    `${host}/assets`,
+  ],
+  category: 'wedding',
 };
 
 export default function RootLayout({
